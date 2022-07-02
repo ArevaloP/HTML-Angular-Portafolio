b1dccc37df963443d55c8900ac8948f9f3eb2688
Georffrey Arevalo <arevalogeorffrey@gmail.com>
2022-07-01 22:37:55 -0500

    Informacion del equipo a través de Google Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Equipo } from '../interfaces/equipo.interface';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
@@ -10,13 +11,29 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada: boolean = false;
+  equipo: Equipo[] = [];
 
   constructor(private http: HttpClient) {
     //Leer archivo JSON
+    this.cargarInfo();
+
+    this.cargarEquipo();
+  }
+
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
         .subscribe((resp: InfoPagina)=>{
           this.cargada = true;
           this.info = resp;
         });
   }
+
+  private cargarEquipo(){
+    this.http.get<Equipo[]>('https://angular-html-92429-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe((resp: Equipo[])=>{
+          this.equipo = resp;
+        });
+  }
+
 }
